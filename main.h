88f154bc9c9b810dba3cb4e3a5c8e9f00253d706
MsWaweru <waweruann92@gmail.com>
2023-09-16 02:20:15 -0700

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <string.h>
+
 int _printf(const char *format, ...);
 
 #endif

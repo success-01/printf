e02cd1f3b8e638cee49936fd34cd06e213a2b511
MsWaweru <waweruann92@gmail.com>
2023-09-17 06:53:24 -0700

    changes

--- main.h
+++ main.h
@@ -8,6 +8,24 @@
 #include <stdarg.h>
 #include <string.h>
 
+/**
+ * struct convert - symbols structure and functions is defined
+ *
+ * @sym: Operator
+ * @f: Associated function
+ */
+
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+
+typedef struct convert conver_t;
+
+/*Main functions*/
+
 int _printf(const char *format, ...);
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 
 #endif

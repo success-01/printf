a15e60bfef57a9e4dea6006273daa99b7c964ee4
success-01 <nompumeleloxaba972@gmail.com>
2023-09-17 08:21:24 -0700

    main

--- main.h
+++ main.h
@@ -8,6 +8,18 @@
 #include <stdarg.h>
 #include <string.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+#define S_LONG 2
+#define S_SHORT 1
+
 /**
  * struct convert - symbols structure and functions is defined
  *
